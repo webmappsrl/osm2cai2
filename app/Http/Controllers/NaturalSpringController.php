0783d266abd055d6955d5d3121c831ccf001f910
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-04-24 09:50:34 +0200

    feat: added model natural springs

---
+++ app/Http/Controllers/NaturalSpringController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreNaturalSpringRequest;
+use App\Http\Requests\UpdateNaturalSpringRequest;
+use App\Models\NaturalSpring;
+
+class NaturalSpringController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreNaturalSpringRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(NaturalSpring $naturalSpring)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(NaturalSpring $naturalSpring)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateNaturalSpringRequest $request, NaturalSpring $naturalSpring)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(NaturalSpring $naturalSpring)
+    {
+        //
+    }
+}
