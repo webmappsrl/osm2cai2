fd3decfd0bd06f7d9c62d0b97888a7f54a0f063a
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-05-03 15:37:47 +0200

    feat: implemented wm-osmfeatures configuration

---
+++ database/migrations/2024_05_03_091655_alter_municipalities_table_alter_column_name_set_nullable.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('municipalities', function (Blueprint $table) {
+            $table->string('name')->nullable()->change();
+            $table->geography('geometry', 'multipolygon', 4326)->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('municipalities', function (Blueprint $table) {
+            $table->string('name')->change();
+            $table->geography('geometry', 'multipolygon', 4326)->change();
+        });
+    }
+};
