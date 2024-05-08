21359e1a20c51b9235d0b90284cb331e38e519d9
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-05-08 15:32:38 +0200

    feat: osm2cai helper class enhancement

--- app/Helpers/Osm2caiHelper.php
+++ app/Helpers/Osm2caiHelper.php
@@ -20,7 +20,7 @@ class Osm2caiHelper
         };
         $osmid = substr($id, 1);
 
-        return 'https://www.openstreetmap.org/'.$finalType.'/'.$osmid;
+        return 'https://www.openstreetmap.org/' . $finalType . '/' . $osmid;
     }
 
     /**
@@ -30,7 +30,7 @@ class Osm2caiHelper
      */
     public static function getOpenstreetmapUrlAsHtml(string $id): string
     {
-        return '<a style="color:darkgreen;" target="_blank" href="'.self::getOpenstreetmapUrl($id).'" target="_blank">'.$id.'</a>';
+        return '<a style="color:darkgreen;" target="_blank" href="' . self::getOpenstreetmapUrl($id) . '" target="_blank">' . $id . '</a>';
     }
 
     /**
@@ -50,5 +50,79 @@ class Osm2caiHelper
         }
 
         return $stars;
+    }
+
+    /**
+     * Get the osmfeatures data correctly formatted as array without the geometry
+     * @param string $data
+     * @return array
+     */
+    public static function getOsmfeaturesDataForNovaDetail(string $data): string
+    {
+        $data = json_decode($data, true);
+        unset($data['type']);
+        unset($data['geometry']);
+        return json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+    }
+
+    /**
+     * It returns RGB string for SAL color according to the following rules
+     * SAL <= 0.2 -> #f1eef6
+     * 0.2 < SAL <= 0.4 -> #bdc9e1
+     * 0.4 < SAL <= 0.6 -> #74a9cf
+     * 0.6 < SAL <= 0.8 -> #2b8cbe
+     * 0.8 < SAL  -> #045a8d
+     *
+     * @param float $sal
+     * @return string
+     */
+    public static function getSalColor(float $sal): string
+    {
+        $color = '';
+        if ($sal <= 0.2) {
+            $color = '#f1eef6';
+        } else if ($sal <= 0.4) {
+            $color = '#bdc9e1';
+        } else if ($sal <= 0.6) {
+            $color = '#74a9cf';
+        } else if ($sal <= 0.8) {
+            $color = '#2b8cbe';
+        } else {
+            $color = '#045a8d';
+        }
+        return $color;
+    }
+
+    /**
+     * 0 969696
+     * 1 FFD23F
+     * 2 B43E8F
+     * 3 1E3888
+     * 4 47AC34
+     *
+     * @param $sda
+     * @return string
+     */
+    public static function getSdaColor($sda): string
+    {
+        $color = '';
+        switch ($sda) {
+            case '0':
+                $color = '#969696';
+                break;
+            case '1':
+                $color = '#FFD23F';
+                break;
+            case '2':
+                $color = '#B43E8F';
+                break;
+            case '3':
+                $color = '#1E3888';
+                break;
+            case '4':
+                $color = '#47AC34';
+                break;
+        }
+        return $color;
     }
 }
