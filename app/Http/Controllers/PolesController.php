d9fc0198fade8a264deda636e91ca1bd01697d9f
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-07-30 13:20:57 +0200

    feat: added poles model

---
+++ app/Http/Controllers/PolesController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorePolesRequest;
+use App\Http\Requests\UpdatePolesRequest;
+use App\Models\Poles;
+
+class PolesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePolesRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Poles $poles)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Poles $poles)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePolesRequest $request, Poles $poles)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Poles $poles)
+    {
+        //
+    }
+}
