d4977a557077fdf0816c5bc7ff9f9d1a79acd5e6
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-07-30 13:21:43 +0200

    feat: poles migration

---
+++ database/migrations/2024_07_30_094956_create_poles_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('poles', function (Blueprint $table) {
+            $table->char('osm_type', 1); // bpchar(1)
+            $table->bigInteger('osm_id'); // int8
+            $table->increments('id'); // int4 with auto-increment
+            $table->text('name')->nullable(); // text with nullable
+            $table->json('tags')->nullable(); // jsonb with nullable
+            $table->geography('geometry', 'point', 4326)->nullable(); // geometry(Point, 3857) with nullable
+            $table->text('ref')->nullable(); // text with nullable
+            $table->text('ele')->nullable(); // text with nullable
+            $table->text('destination')->nullable(); // text with nullable
+            $table->text('support')->nullable(); // text with nullable
+            $table->integer('elevation')->nullable(); // int4 with nullable
+            $table->integer('score')->nullable(); // int4 with nullable
+
+            $table->timestamps(); // includes created_at and updated_at columns
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('poles');
+    }
+};
