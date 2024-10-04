32628fe62610a314c86c2fc8307f5e48c3f45806
Gennaro Manzo <75580783+gemanzo@users.noreply.github.com>
2024-10-04 08:41:07 +0200

    Api hiking_routes/list V2 OC:4056 (#63)
    * chore: added swagger
    
    * chore: first swagger configuration
    
    * chore: configured swagger
    
    * feat: api v2 hiking routes list implemented

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HikingRouteController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,3 +14,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::prefix('v2')->group(function () {
+    Route::get('/hiking-routes/list', [HikingRouteController::class, 'index'])->name('hiking-routes-list');
+});
