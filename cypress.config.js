324ba9fb415d71400e73f0ddd555ae6c81063f9b
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-10-15 14:18:35 +0200

    fixed app url

--- cypress.config.js
+++ cypress.config.js
@@ -8,6 +8,7 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: process.env.APP_URL || 'http://localhost:8008',
     env: {
+      appUrl: process.env.APP_URL || 'http://localhost:8008',
       adminEmail: process.env.ADMIN_EMAIL || 'team@webmapp.it',
       adminPassword: process.env.ADMIN_PASSWORD,
       referentEmail: process.env.REFERENT_EMAIL || 'referenteNazionale@webmapp.it',
