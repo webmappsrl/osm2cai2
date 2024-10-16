b50ed86416b9f4ead02613b8f3e9a7cf3ca23122
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-10-16 11:56:10 +0200

    chore: added test db seeder

--- database/seeders/RegionSeeder.php
+++ database/seeders/RegionSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Region;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class RegionSeeder extends Seeder
 {
@@ -12,6 +14,11 @@ class RegionSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        //create 29 regions with names 
+        $regionNames = 'Auvergne-Rhône-Alpes,Toscana,Campania,Emilia-Romagna,Friuli-Venezia Giulia,Marche,Puglia,Veneto,Calabria,Lazio,Liguria,Molise,Piemonte,Umbria,Ticino,Lombardia,Sicilia,Valle d\'Aosta / Vallée d\'Aoste,Valais/Wallis,Abruzzo,Basilicata,Trentino-Alto Adige/Südtirol,Graubünden/Grischun/Grigioni,Salzburg,Corse,Kärnten,Provence-Alpes-Côte d\'Azur,Tirol,Sardegna';
+        $regions = explode(',', $regionNames);
+        foreach ($regions as $regionName) {
+            Region::create(['name' => $regionName]);
+        }
     }
 }
