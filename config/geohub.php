d0d0ee62ec0f40ebabfb5830e34bcde1e7fe7552
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-10-17 15:43:23 +0200

    feat: added WmNovaFieldsTrait

---
+++ config/geohub.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'geohub_url' => env('GEOHUB_URL', 'https://geohub.webmapp.it'),
+    'configs' => [
+        '26' => 'https://geohub.webmapp.it/api/app/webmapp/26/config.json',
+        '20' => 'https://geohub.webmapp.it/api/app/webmapp/20/config.json',
+        '58' => 'https://geohub.webmapp.it/api/app/webmapp/58/config.json',
+    ],
+];
