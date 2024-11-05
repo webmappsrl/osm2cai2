071b028c4c961cf7d18414f4599459ea92fa0ea3
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-11-05 12:07:10 +0100

    feat: ugcPois api for app

--- routes/api.php
+++ routes/api.php
@@ -32,6 +32,28 @@ Route::name('api.')->group(function () {
     });
 });
 
+Route::group([
+    'middleware' => 'auth.jwt',
+], function () {
+    Route::prefix('ugc')->name('ugc.')->group(function () {
+        Route::prefix('poi')->name('poi.')->group(function () {
+            Route::post("store", [UgcPoiController::class, 'store'])->name('store');
+            Route::get("index", [UgcPoiController::class, 'index'])->name('index');
+            Route::delete('delete/{id}', [UgcPoiController::class, 'destroy'])->name('destroy');
+        });
+        Route::prefix('track')->name('track.')->group(function () {
+            Route::post("store", [UgcTrackController::class, 'store'])->name('store');
+            Route::get("index", [UgcTrackController::class, 'index'])->name('index');
+            Route::delete('delete/{id}', [UgcTrackController::class, 'destroy'])->name('destroy');
+        });
+        Route::prefix('media')->name('media.')->group(function () {
+            Route::post("store", [UgcMediaController::class, 'store'])->name('store');
+            Route::get("index", [UgcMediaController::class, 'index'])->name('index');
+            Route::delete('delete/{id}', [UgcMediaController::class, 'destroy'])->name('destroy');
+        });
+    });
+});
+
 Route::prefix('v2')->group(function () {
     Route::get('/hiking-routes/list', [HikingRouteController::class, 'index'])->name('hiking-routes-list');
 });
