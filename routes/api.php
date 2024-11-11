18faadf4c9d8647fd450c20e554411c7e62b4c68
Gennaro Manzo <75580783+gemanzo@users.noreply.github.com>
2024-11-11 08:31:29 +0100

    feat: api integration OC:4226 (#73)
    * chore: spatial data trait
    
    * chore: refactored models to use spatialdata trait
    
    * feat: csv, kml, shapefile apis
    
    * feat: added ogr2og2 in docker container for shapefile generation
    
    * feat: geojson api
    
    * chore: added sections to geojson controller
    
    * chore: added code column to regions table
    
    * Updated submodule wm-package
    
    * Updated submodule wm-osmfeatures
    
    * Updated submodule wm-package
    
    * fix: fixed base job extended class
    
    * feat: api v1
    
    * fix: hikingroute osm list fix
    
    * fix: typo

--- routes/api.php
+++ routes/api.php
@@ -79,3 +79,29 @@ Route::prefix('v2')->group(function () {
         Route::get('/overlay.geojson', [SourceSurveyController::class, 'overlayGeoJson'])->name('overlay-geojson');
     });
 });
+
+
+Route::prefix('csv')->name('csv.')->group(function () {
+    Route::get('/{modelType}/{id}', [CsvController::class, 'download']);
+});
+//TODO check compatibility with geography type geometry
+Route::prefix('shapefile')->name('shapefile.')->group(function () {
+    Route::get('/{modelType}/{id}', [ShapeFileController::class, 'download']);
+});
+//TODO check compatibility with geography type geometry
+Route::prefix('kml')->name('kml.')->group(function () {
+    Route::get('/{modelType}/{id}', [KmlController::class, 'download']);
+});
+Route::prefix('geojson')->name('geojson.')->group(function () {
+    Route::get('/{modelType}/{id}', [GeojsonController::class, 'download']);
+});
+
+Route::prefix('v1')->name('v1')->group(function () {
+    Route::get('/hiking-routes/region/{regione_code}/{sda}', [HikingRoutesRegionControllerV1::class, 'hikingroutelist'])->name('hr-ids-by-region');
+    Route::get('/hiking-routes-osm/region/{regione_code}/{sda}', [HikingRoutesRegionControllerV1::class, 'hikingrouteosmlist'])->name('hr_osmids_by_region');
+    Route::get('/hiking-route/{id}', [HikingRoutesRegionControllerV1::class, 'hikingroutebyid'])->name('hr_by_id');
+    Route::get('/hiking-route-osm/{id}', [HikingRoutesRegionControllerV1::class, 'hikingroutebyosmid'])->name('hr_by_osmid');
+    Route::get('/hiking-routes/bb/{bounding_box}/{sda}', [HikingRoutesRegionControllerV1::class, 'hikingroutelist_bb'])->name('hr-ids-by-bb');
+    Route::get('/hiking-routes-osm/bb/{bounding_box}/{sda}', [HikingRoutesRegionControllerV1::class, 'hikingrouteosmlist_bb'])->name('hr-osmids-by-bb');
+    Route::get('/hiking-routes-collection/bb/{bounding_box}/{sda}', [HikingRoutesRegionControllerV1::class, 'hikingroutelist_collection'])->name('hr-collection-by-bb');
+});
