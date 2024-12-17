60a59a77e5be170aab1d40acc78fcb9dc2cedaf6
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-12-17 13:08:51 +0100

    fix: missing class in blade view for passing e2e tests

--- resources/views/nova/cards/sal-nazionale.blade.php
+++ resources/views/nova/cards/sal-nazionale.blade.php
@@ -1,4 +1,5 @@
-<div style="background-color: {{ $backgroundColor }}; color: white; font-size: xx-large; padding: 1rem 0">
+<div class="sal-nazionale-card"
+    style="background-color: {{ $backgroundColor }}; color: white; font-size: xx-large; padding: 1rem 0">
     {{ number_format($sal * 100, 2) }}%
 </div>
 <h6>SAL Nazionale</h6>
