9d088b3c8d6a29ff164f2fd0d892cc067a742cb4
gemanzo <gemanzo@users.noreply.github.com>
2024-12-19 08:11:20 +0000

    Check and fix styling

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,7 +15,6 @@ use Wm\WmPackage\Models\User as WmUser;
 
 class User extends WmUser
 {
-
     /**
      * The attributes that are mass assignable.
      *
@@ -29,7 +28,6 @@ class User extends WmUser
         'club_cai_code',
         'phone',
     ];
-
 
     public function EcPois()
     {
--- app/Nova/User.php
+++ app/Nova/User.php
@@ -26,8 +26,6 @@ class User extends AbstractUser
      */
     public static $model = \App\Models\User::class;
 
-
-
     /**
      * Get the fields displayed by the resource.
      *
@@ -62,10 +60,11 @@ class User extends AbstractUser
                 ->sortable(),
 
         ];
+
         return [
             ...array_slice($baseFields, 0, 5),
             ...$relationFields,
-            ...array_slice($baseFields, 4)
+            ...array_slice($baseFields, 4),
         ];
     }
 
