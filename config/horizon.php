b7bfb4ec97f11ddd111bf5a6840a1b335637aceb
Gennaro Manzo <gennaromanzo1993@gmail.com>
2025-01-09 13:39:18 +0100

    chore: updated horizon config

--- config/horizon.php
+++ config/horizon.php
@@ -56,7 +56,7 @@ return [
 
     'prefix' => env(
         'HORIZON_PREFIX',
-        Str::slug(env('APP_NAME', 'laravel'), '_').'_horizon:'
+        Str::slug(env('APP_NAME', 'laravel'), '_') . '_horizon:'
     ),
 
     /*
@@ -207,8 +207,8 @@ return [
                 'connection' => 'redis',
                 'queue' => ['geometric-computations'],
                 'balance' => 'simple',
-                'maxProcesses' => 5,
-                'tries' => 1,
+                'maxProcesses' => 15,
+                'tries' => 2,
                 'timeout' => 300,
             ],
         ],
