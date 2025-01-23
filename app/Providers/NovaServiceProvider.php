5312c43a6e75ed371fc9ac48c8a4690fc1c774fa
Gennaro Manzo <gennaromanzo1993@gmail.com>
2025-01-23 15:16:54 +0100

    fix: fixed nova gate

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -191,9 +191,7 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     protected function gate()
     {
         Gate::define('viewNova', function ($user) {
-            return in_array($user->email, [
-                'team@webmapp.it',
-            ]);
+            return true;
         });
     }
 
