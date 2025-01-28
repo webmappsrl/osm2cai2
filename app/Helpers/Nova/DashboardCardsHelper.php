8ffd6a387c0685b0e807536a730816cbe51cf8ab
Gennaro Manzo <gennaromanzo1993@gmail.com>
2025-01-28 09:50:11 +0100

    chore: updated italy dashboard with caching methods in dashboard helper

--- app/Helpers/Nova/DashboardCardsHelper.php
+++ app/Helpers/Nova/DashboardCardsHelper.php
@@ -12,11 +12,13 @@ class DashboardCardsHelper
 {
     public function getNationalSalCard()
     {
-        $sal = (HikingRoute::where('osm2cai_status', 1)->count() * 0.25 +
-            HikingRoute::where('osm2cai_status', 2)->count() * 0.50 +
-            HikingRoute::where('osm2cai_status', 3)->count() * 0.75 +
-            HikingRoute::where('osm2cai_status', 4)->count()
-        ) / Region::sum('num_expected');
+        $sal = cache()->remember('national_sal', now()->addDays(2), function () {
+            return (HikingRoute::where('osm2cai_status', 1)->count() * 0.25 +
+                HikingRoute::where('osm2cai_status', 2)->count() * 0.50 +
+                HikingRoute::where('osm2cai_status', 3)->count() * 0.75 +
+                HikingRoute::where('osm2cai_status', 4)->count()
+            ) / Region::sum('num_expected');
+        });
 
         return (new HtmlCard())
             ->width('1/4')
@@ -30,9 +32,9 @@ class DashboardCardsHelper
 
     private function getTotalKmCard($status, $label)
     {
-        $cacheKey = is_array($status) ? 'total_km_'.implode('_', $status) : 'total_km_'.$status;
+        $cacheKey = is_array($status) ? 'total_km_' . implode('_', $status) : 'total_km_' . $status;
 
-        $total = cache()->remember($cacheKey, now()->addDay(), function () use ($status) {
+        $total = cache()->remember($cacheKey, now()->addDays(2), function () use ($status) {
             $query = DB::table('hiking_routes')
                 ->selectRaw('
                     COALESCE(
@@ -80,5 +82,67 @@ class DashboardCardsHelper
             ->view('nova.cards.no-permissions-card')
             ->center()
             ->withBasicStyles();
+    }
+
+    public function getItalyDashboardCards()
+    {
+        $numbers = cache()->remember('italy_dashboard_data', now()->addDays(2), function () {
+            $values = DB::table('hiking_routes')
+                ->select('osm2cai_status', DB::raw('count(*) as num'))
+                ->groupBy('osm2cai_status')
+                ->get();
+
+            $numbers = [1 => 0, 2 => 0, 3 => 0, 4 => 0];
+
+            foreach ($values as $value) {
+                $numbers[$value->osm2cai_status] = $value->num;
+            }
+
+            return $numbers;
+        });
+
+        $tot = array_sum($numbers);
+
+        return [
+            'italy-total' => (new HtmlCard())->width('1/4')
+                ->view('nova.cards.italy-total', ['tot' => $tot])
+                ->center()
+                ->withBasicStyles(),
+            'sda1' => (new HtmlCard())->width('1/4')
+                ->view('nova.cards.italy-sda', [
+                    'number' => $numbers[1],
+                    'sda' => 1,
+                    'backgroundColor' => Osm2caiHelper::getSdaColor(1),
+                ])
+                ->center()
+                ->withBasicStyles(),
+
+            'sda2' => (new HtmlCard())->width('1/4')
+                ->view('nova.cards.italy-sda', [
+                    'number' => $numbers[2],
+                    'sda' => 2,
+                    'backgroundColor' => Osm2caiHelper::getSdaColor(2),
+                ])
+                ->center()
+                ->withBasicStyles(),
+
+            'sda3' => (new HtmlCard())->width('1/4')
+                ->view('nova.cards.italy-sda', [
+                    'number' => $numbers[3],
+                    'sda' => 3,
+                    'backgroundColor' => Osm2caiHelper::getSdaColor(3),
+                ])
+                ->center()
+                ->withBasicStyles(),
+
+            'sda4' => (new HtmlCard())->width('1/4')
+                ->view('nova.cards.italy-sda', [
+                    'number' => $numbers[4],
+                    'sda' => 4,
+                    'backgroundColor' => Osm2caiHelper::getSdaColor(4),
+                ])
+                ->center()
+                ->withBasicStyles(),
+        ];
     }
 }
