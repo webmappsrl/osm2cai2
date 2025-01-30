670ec740bfb0c0a5c5342f6615a3c958895b3b27
Gennaro Manzo <gennaromanzo1993@gmail.com>
2025-01-30 13:44:08 +0100

    fix: number formatting on sal nazionale italy dashbaord

--- app/Nova/Dashboards/Main.php
+++ app/Nova/Dashboards/Main.php
@@ -29,6 +29,24 @@ class Main extends Dashboard
 
     public function name()
     {
+        $user = Auth::user();
+
+        if ($user->hasRole('Administrator')) {
+            return __('Main Dashboard');
+        }
+
+        if ($user->hasRole('National Referent')) {
+            return __('National Dashboard');
+        }
+
+        if ($user->hasRole('Regional Referent')) {
+            return __('Regional Dashboard');
+        }
+
+        if ($user->hasRole('Local Referent')) {
+            return __('Local Dashboard');
+        }
+
         return __('Main Dashboard');
     }
 
