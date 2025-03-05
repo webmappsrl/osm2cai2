9fb5a6415185f337cbfe3c9c6a0774b80785e992
FalconClaws059 <valerio.bertolone1998@gmail.com>
2025-03-05 09:50:54 +0100

    feat: Added tests for AreaModelService and factory for Area oc:4552

---
+++ tests/Unit/Services/AreaModelServiceTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Unit\Services;
+
+use Tests\TestCase;
+use App\Models\Area;
+use App\Services\AreaModelService;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+class AreaModelServiceTest extends TestCase
+{
+    use DatabaseTransactions;
+    protected $area;
+    protected $areaService;
+
+    const WKT_POLYGON = 'POLYGON((0 0, 10 0, 10 10, 0 10, 0 0))';
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->area = Area::factory()->create();
+
+        $this->area->children()->createMany([
+            [
+                'code' => 'B',
+                'name' => 'Test Area 1',
+                'full_code' => '472B',
+                'geometry' => self::WKT_POLYGON,
+                'num_expected' => 1,
+            ],
+        ]);
+
+        $this->areaService = new AreaModelService();
+    }
+    
+    protected function convertPolygonToWKB(string $wktPolygon): string
+    {
+        $result = DB::selectOne(
+            'SELECT ST_AsEWKB(ST_GeomFromText(?, 4326)) as wkb',
+            [$wktPolygon]
+        );
+        $wkb = is_resource($result->wkb) ? stream_get_contents($result->wkb) : $result->wkb;
+        return strtoupper(bin2hex($wkb));
+    }
+    
+    public function testComputeGeometryBySectors()
+    {
+        $this->assertEquals(
+            $this->convertPolygonToWKB(self::WKT_POLYGON), 
+            $this->areaService->computeGeometryBySectors($this->area)
+        );
+    }
+}
