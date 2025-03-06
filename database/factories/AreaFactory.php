e88660da089aa373520c5dec9a9b216e34069a3c
FalconClaws059 <83666067+FalconClaws059@users.noreply.github.com>
2025-03-06 08:02:28 +0100

    feat: Added tests for AreaModelService and factory for Area oc:4552 (#155)
    * feat: Added tests for AreaModelService and factory for Area oc:4552
    
    * fix: deleted unused submodules oc:4552

--- database/factories/AreaFactory.php
+++ database/factories/AreaFactory.php
@@ -17,7 +17,11 @@ class AreaFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'code' => $this->faker->regexify('[A-Z]{1}'),
+            'name' => $this->faker->word,
+            'geometry' => "POLYGON((0 0, 10 0, 10 10, 0 10, 0 0))",
+            'full_code' => $this->faker->regexify('[A-Z]{4}'),
+            'num_expected' => $this->faker->numberBetween(1, 100),
         ];
     }
 }
