b686d47698b8f11bbea6d1e7e3d4a90c49d35cb9
Gennaro Manzo <gennaromanzo1993@gmail.com>
2025-04-16 09:42:11 +0200

    Refactor deployment scripts: update deploy_dev.sh for production deployment and remove unused local deployment scripts

--- scripts/deploy_dev.sh
+++ scripts/deploy_dev.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
 set -e
 
-echo "Deployment started ..."
+echo "Production deployment started ..."
 
-# Enter maintenance mode or return true
-# if already is in maintenance mode
-(php artisan down) || true
+php artisan down
 
 git submodule update --init --recursive
 
-# Install composer dependencies
-composer install  --no-interaction --prefer-dist --optimize-autoloader
+composer install --no-interaction --prefer-dist --optimize-autoloader
+composer dump-autoload
 
 # Update nova assets for the custom login page
 # https://github.com/Muetze42/nova-assets-changer
 php artisan nova:custom-assets
 
-# Run database migrations
-php artisan migrate
-
-# Create admin user
-php artisan db:seed
-
-php artisan optimize:clear
+# Clear and cache config
+php artisan config:cache
 php artisan config:clear
 
-# Exit maintenance mode
+# Clear the old cache
+php artisan clear-compiled
+
+php artisan optimize
+
+php artisan migrate --force
+
+# gracefully terminate laravel horizon
+php artisan horizon:terminate
+
 php artisan up
 
 echo "Deployment finished!"
