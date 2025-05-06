d0a72075f8cd4bd0ed146092ee03679fcb403576
Gennaro Manzo <75580783+gemanzo@users.noreply.github.com>
2025-05-06 10:28:08 +0200

    feat: added hiking route club association action in club nova resource (#188)
    - Modified the HikingRoute model to dispatch jobs on creation and save events, improving the workflow for syncing club associations.
    - Added a new action, FindClubHrAssociationAction, to facilitate the association of clubs with hiking routes from the Nova interface.

---
+++ app/Nova/Actions/FindClubHrAssociationAction.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Nova\Actions;
+
+use App\Jobs\SyncClubHikingRouteRelationJob;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Collection;
+use Laravel\Nova\Actions\Action;
+use Laravel\Nova\Actions\ActionResponse;
+use Laravel\Nova\Fields\ActionFields;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class FindClubHrAssociationAction extends Action
+{
+    use InteractsWithQueue;
+    use Queueable;
+
+    public $name = 'Find Club Hiking Route Association';
+
+    /**
+     * Indicates if this action can be run without any models.
+     *
+     * @var bool
+     */
+    public $standalone = false;
+
+    /**
+     * Indicates if this action is only available on the resource detail view.
+     *
+     * @var bool
+     */
+    public $onlyOnDetail = true;
+
+    /**
+     * Perform the action on the given models.
+     *
+     * @return mixed
+     */
+    public function handle(ActionFields $fields, Collection $models)
+    {
+        if ($models->count() !== 1) {
+            return Action::danger('This action can only be executed on one resource at a time.');
+        }
+
+        $model = $models->first();
+        $modelType = class_basename(get_class($model));
+
+        // Dispatch the job to associate clubs to hiking routes
+        SyncClubHikingRouteRelationJob::dispatch($modelType, $model->id);
+
+        return Action::message('Association process has been started for this ' . $modelType);
+    }
+
+    /**
+     * Get the fields available on the action.
+     *
+     * @return array<int, \Laravel\Nova\Fields\Field>
+     */
+    public function fields(NovaRequest $request): array
+    {
+        return [];
+    }
+}
