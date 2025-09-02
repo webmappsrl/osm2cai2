323a271bd6c5d807d07774e85f051587fafbd263
Carla Cupani <cpnclp00@gmail.com>
2025-09-02 16:26:10 +0200

    feat(routes): ✨ add support for poles in feature collection map URL OC:6134
    - Introduced a new model parameter in the 'widget/feature-collection-map-url' route to support dynamic model selection.
    - Added logic to handle 'poles' model, fetching its GeoJSON feature collection map.
    - Adjusted the route to switch between 'hiking-route' and 'poles' based on the model parameter, allowing for flexible data retrieval.
    - Ensured backward compatibility by defaulting to null GeoJSON for unrecognized models, preventing errors.

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\HikingRouteLoScarponeExportController;
 use App\Http\Controllers\MigrationCheck;
 use App\Http\Controllers\MiturAbruzzoController;
 use App\Models\HikingRoute;
+use App\Models\Poles;
 use Illuminate\Support\Facades\Route;
 
 /*
