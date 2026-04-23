4b687459aa11b203bbbf598cdeaea04192b89980
peppedeka <peppedeka@gmail.com>
2026-04-23 13:59:51 +0200

    fix(HikingRoute): 🐛 add explicit geometry casting in SQL query
    - Added explicit casting of `hr.geometry` to `geometry` type in the SQL queries for both `getFromInfo` and `getToInfo` methods.
    - This change ensures that the spatial functions `ST_StartPoint` and `ST_LineMerge` receive the correct geometry type input, preventing potential errors related to type inference in database operations.
    - The explicit casting improves the robustness of the SQL queries by aligning the data type expectations of the geometry processing functions.

--- app/Models/HikingRoute.php
+++ app/Models/HikingRoute.php
@@ -761,7 +761,7 @@ class HikingRoute extends EcTrack
         municipalities as m,
         hiking_routes as hr
     WHERE
-        st_intersects(m.geometry, ST_Transform(ST_StartPoint(hr.geometry), 4326))
+        st_intersects(m.geometry, ST_Transform(ST_StartPoint(hr.geometry::geometry), 4326))
         AND hr.id = {$this->id};
 SQL;
 
@@ -818,7 +818,7 @@ SQL;
         municipalities as m,
         hiking_routes as hr
     WHERE
-        st_intersects(m.geometry, ST_Transform(ST_Endpoint(ST_LineMerge(hr.geometry)), 4326))
+        st_intersects(m.geometry, ST_Transform(ST_Endpoint(ST_LineMerge(hr.geometry::geometry)), 4326))
         AND hr.id = {$this->id};
 SQL;
 
